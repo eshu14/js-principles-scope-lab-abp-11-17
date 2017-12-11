c717ed7d4ba192c5fb685a14f9a9f86afa9817f4
Easwari Ramani <eshuprad@gmail.com>
2017-12-11 13:46:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+var customerName;
+function upperCaseCustomerName(cust){
+  return cust.toUpperCase();
+}
+function setBestCustomer(){
+  var bestCustomer='not bob';
+}
